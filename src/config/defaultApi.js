2f22776b1c1e6afb58e466fc4c693b8e2c46642f
nguyenl3v <nguyenka1358@gmail.com>
2019-11-24 21:10:00 +0700

    fix default api

--- src/config/defaultApi.js
+++ src/config/defaultApi.js
@@ -1,4 +1,4 @@
 export const API =
-  process.env.NODE_ENV !== "production"
-    ? "http://localhost:4001"
-    : "https://server-cms-api.herokuapp.com";
+  process.env.NODE_ENV === "production"
+    ? "https://server-cms-api.herokuapp.com"
+    : "http://localhost:4001";
