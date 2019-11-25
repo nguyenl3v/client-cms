71e88440ddbc4322deb8a8ff53effc9c7eb5486e
nguyenl3v <nguyenka1358@gmail.com>
2019-11-25 08:32:32 +0700

    fix bug loading

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -1,14 +1,17 @@
 import React from "react";
 import loading from "../asset/tenor.gif";
 import { connect } from "react-redux";
+import styled from "styled-components";
 
-function Loading({loadings}) {
+function Loading({ loadings }) {
   if (loadings) {
     return (
-      <div className="loading">
+      <StyledLoading>
         <img src={loading} alt="loading" />
-      </div>
+      </StyledLoading>
     );
+  } else {
+    return <React.Fragment></React.Fragment>;
   }
 }
 const mapStateToProps = state => {
@@ -17,7 +20,21 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(
-  mapStateToProps,
-  null
-)(Loading);
+export default connect(mapStateToProps, null)(Loading);
+const StyledLoading = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.4);
+  width: 100%;
+  height: 100vh;
+  z-index: 9999;
+  img {
+    width: 70px;
+    height: 70px;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+`;
